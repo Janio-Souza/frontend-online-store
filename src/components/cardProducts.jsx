5b019f03c0755fa43dde2d8e0ff1544044f4f65f
Thalis Venancio <devthalis@gmail.com>
2022-09-09 19:56:47 -0300

    analisando erro

---
+++ src/components/cardProducts.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// class cardProducts extends React.Component {
+//   render() {
+//     // const { name, image, price } = this.props;
+//     return (
+//       // <div>
+//       //   <p>{ name }</p>
+//       //   <img src={ image } alt={ name } />
+//       //   <p>{ price }</p>
+//       // </div>
+//       <p>TEST</p>
+//     );
+//   }
+// }
+
+// // cardProducts.propTypes = {
+// //   name: PropTypes.string,
+// //   image: PropTypes.string,
+// //   price: PropTypes.string,
+// // }.isRequired;
+
+// export default cardProducts;
+export default class cardProducts extends React.Component {
+  render() {
+    return (
+      <p>TESTE</p>
+    );
+  }
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import Categories from '../components/categories';
-import { getCategories } from '../services/api';
+// import cardProducts from '../components/cardProducts';
 
 export default class Home extends React.Component {
   state = {
-    products: [],
     message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
     categories: [],
+    allProducts: [],
+    inputText: '',
   };
 
   async componentDidMount() {
     const categories = await getCategories();
     this.setState({ categories });
-    console.log('log categorias', categories);
   }
 
+  handleClick = async (categoryId) => {
+    const { inputText } = this.state;
+    const allProducts = await getProductsFromCategoryAndQuery(inputText, categoryId);
+    this.setState({ allProducts: allProducts.results });
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ inputText: value });
+  };
+
   render() {
-    const { products, message, categories } = this.state;
+    const { message, categories, allProducts } = this.state;
+    console.log(allProducts);
     return (
       <div>
-        <div className="home">
-          <input type="text" placeholder="Pesquisar Produto" />
-          {
-            products.length === 0
-              ? <p data-testid="home-initial-message">{ message }</p> : null
-          }
-        </div>
+        <input
+          type="text"
+          placeholder="Pesquisar Produto"
+          data-testid="query-input"
+          onChange={ this.handleChange }
+        />
+
+        <button
+          type="button"
+          value="Pesquisar"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
+
+        <button
+          type="button"
+        >
+          <Link to="shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
+        </button>
+
+        {
+          allProducts.length === 0
+            ? <p data-testid="home-initial-message">{ message }</p> : null
+        }
+        {/* {
+          allProducts.map((item) => (
+            <cardProducts
+              key={ item.id }
+              name={ item.title }
+              image={ item.thumbnail }
+              price={ item.price }
+            />
+          ))
+        } */}
+
         <p>Categorias</p>
         {
           categories.map((element) => (
@@ -37,11 +80,6 @@ export default class Home extends React.Component {
             />
           ))
         }
-        <button
-          type="submit"
-        >
-          <Link to="shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
-        </button>
       </div>
     );
   }
