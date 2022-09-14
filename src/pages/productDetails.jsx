2eea246e19cc748ce30139d61554895bdb8c4ff2
Marco Fabian <marcofabianufmg@hotmail.com>
2022-09-14 11:53:46 -0300

    Requisito 9

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -14,12 +14,43 @@ export default class ProductyDetails extends React.Component {
     this.setState({ productDetails });
   }
 
+  addStorage = async () => {
+    const { productDetails } = this.state;
+
+    if (!JSON.parse(localStorage.getItem('itens'))) {
+      localStorage.setItem('itens', JSON.stringify([]));
+    }
+
+    const checkLocalStorage = JSON.parse(localStorage.getItem('itens'));
+
+    if (productDetails) {
+      const { id, title: name, price, thumbnail: image } = productDetails;
+
+      const product = { id, name, price, image };
+
+      const productShopping = checkLocalStorage.find(
+        (item) => item[0].id === productDetails.id,
+      );
+
+      if (productShopping === undefined) {
+        localStorage.setItem('itens', JSON.stringify(
+          [...checkLocalStorage, [product, 1]],
+        ));
+      } else {
+        const index = checkLocalStorage
+          .findIndex((search) => search[0] === productShopping[0]);
+        checkLocalStorage[index][1] += 1;
+        localStorage.setItem('itens', JSON.stringify(checkLocalStorage));
+      }
+    }
+  };
+
   render() {
     const { productDetails } = this.state;
     return (
       <div>
         <section>
-          <h2 data-testid="product-detail-name">{ productDetails.title }</h2>
+          <h2 data-testid="product-detail-name">{productDetails.title}</h2>
           <img
             data-testid="product-detail-image"
             src={ productDetails.thumbnail }
@@ -27,7 +58,7 @@ export default class ProductyDetails extends React.Component {
           />
           <p data-testid="product-detail-price">
             R$
-            { ` ${productDetails.base_price}` }
+            {` ${productDetails.base_price}`}
           </p>
           <button
             type="button"
@@ -41,8 +72,17 @@ export default class ProductyDetails extends React.Component {
             {` ${productDetails.condition}`}
           </p>
           <p>
-            {`${productDetails.warranty}`}
+            {productDetails.warranty}
           </p>
+
+          <button
+            type="button"
+            onClick={ this.addStorage }
+            data-testid="product-detail-add-to-cart"
+          >
+            Adicionar ao carrinho
+          </button>
+
         </section>
       </div>
     );
